8026d8149c7f6464d75f46245845d697506ddfe4
Anders Bech Mellson <anders@stately.ai>
2024-01-15 12:16:19 +0100

    Auto commit from Stately Studio

--- machines/test.ts
+++ machines/test.ts
@@ -1,3 +1,14 @@
 import { createMachine } from "xstate";
 
-const machine = createMachine({});
+const machine = createMachine({
+/** @xstate-layout N4IgpgJg5mDOIC5QBc7IHQDkwHcAEsyAhqngIxkDEAxgDYCW1A1gNoAMAuoqAA4D2sesnp8AdtxAAPRAGYAbOgAcZAEwBWADQgAnojJsAnAF8TW0XwhwJqQhP6DhYidIQBaFQBZ0ctXPVbdNxkvNVMQGwxsfEISMHIyOwEhEXEkKUQVGXRgmQB2TR1EDyyC5D4eIpKtWjAAM2QMrJz8rQAneigACwaEMmLs4paQHHoIZE69FTYtACM+ZDKAW0QDMnQCzrAO7qLFExMgA */
+  states: {
+    "New state 11": {
+      on: {
+        click: "New state 11"
+      }
+    }
+  },
+
+  initial: "New state 11"
+});
